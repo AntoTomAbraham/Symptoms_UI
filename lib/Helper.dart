81cd326518c9441406c52bd911ed89d60a498463
Anto Tom Abraham <antotom53@gmail.com>
2021-04-09 16:22:10 +0530

    data work

---
+++ lib/Helper.dart
@@ -0,0 +1,10 @@
+List<String> vomiting = [
+  "vomited once during the day",
+  "vomited 2-5 times during the day",
+  "vomited 6 or more times during a day",
+];
+List<String> fatique = [
+  "Able to do most activities",
+  "in bed less than 50% of the day",
+  "in bed more than 50% of the day",
+];
